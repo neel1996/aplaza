d68c8fcccdb618c1f8dc0962016b0ccde6fc9ca6
Neel <neelusenthil1996@gmail.com>
2020-12-23 23:14:16 +0530

    add a project add action button

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,11 @@
+import AddProjectButtonComponent from "../components/AddProject/AddProjectButtonComponent";
 import HeaderSummary from "../components/Header/HeaderSummary";
 import "../styles/globals.css";
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <HeaderSummary></HeaderSummary>
+      <AddProjectButtonComponent></AddProjectButtonComponent>
     </>
   );
 }
