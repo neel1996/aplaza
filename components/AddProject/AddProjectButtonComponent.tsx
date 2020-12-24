7b21f3257ee7ebc8b27e16e8a7dd895d969f7749
Neel <neelusenthil1996@gmail.com>
2020-12-24 22:12:43 +0530

    enclose add button in next link

--- components/AddProject/AddProjectButtonComponent.tsx
+++ components/AddProject/AddProjectButtonComponent.tsx
@@ -1,18 +1,21 @@
-import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import Link from "next/link";
 
 export default function AddProjectButtonComponent() {
   library.add(fas);
 
   return (
-    <div className="flex fixed bottom-0 right-0 mr-6 my-6 justify-center align-middle items-center rounded-full w-20 h-20 bg-green-500 border-8 border-green-300 transition-all hover:shadow-lg hover:border-green-200 cursor-pointer">
-      <div className="flex w-full h-full mx-auto my-auto justify-center items-center align-middle font-sans font-semibold text-green-100">
-        <div>
-          <FontAwesomeIcon icon={["fas", "plus"]} size="2x"></FontAwesomeIcon>
+    <Link href="/addproject">
+      <div className="flex fixed bottom-0 right-0 mr-6 my-6 justify-center align-middle items-center rounded-full w-20 h-20 bg-green-500 border-8 border-green-300 transition-all hover:shadow-lg hover:border-green-200 cursor-pointer">
+        <div className="flex w-full h-full mx-auto my-auto justify-center items-center align-middle font-sans font-semibold text-green-100">
+          <div>
+            <FontAwesomeIcon icon={["fas", "plus"]} size="2x"></FontAwesomeIcon>
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 }
