261e35e222c7809396882464d7b5238497647d14
Neel <neelusenthil1996@gmail.com>
2020-12-27 19:15:25 +0530

    add new data file dedicated for project count

--- pages/api/projectcount.ts
+++ pages/api/projectcount.ts
@@ -4,12 +4,11 @@ import path from "path";
 export default function projectcount(req, res) {
   const projectDataPath = path.join(
     process.cwd(),
-    "pages/api/data/projectData.json"
+    "pages/api/data/projectCountData.json"
   );
 
   const projectDataString = fs.readFileSync(projectDataPath).toString("utf-8");
   const projectData = JSON.parse(projectDataString);
-  console.log(projectData);
 
-  res.json(projectData[0].projectCountDetails);
+  res.json(projectData[0]);
 }
