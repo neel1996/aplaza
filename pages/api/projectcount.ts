df65b527ccdc653d4509d9a546ce8d5b25f7a185
Neel <neelusenthil1996@gmail.com>
2020-12-28 18:31:44 +0530

    remove unwanted dependencies and imports

--- pages/api/projectcount.ts
+++ pages/api/projectcount.ts
@@ -1,5 +1,3 @@
-import fs from "fs";
-import path from "path";
 import { StorageConnectionFactory } from "./data/storageConnectorFactory/StorageConnectorFactory";
 import ProjectCountClass from "./projectCount/ProjectCountClass";
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,12 @@
-import Head from "next/head";
-import { useContext } from "react";
 import AddProjectButtonComponent from "../components/AddProject/AddProjectButtonComponent";
-import AddProjectFormComponent from "../components/AddProject/AddProjectFormComponent";
-import { AplazaContext } from "../components/context";
 import HeaderSummary from "../components/Header/HeaderSummary";
+import ProjectComponent from "../components/ProjectComponent/ProjectComponent";
 
 export default function Home() {
-  const { state } = useContext(AplazaContext);
   return (
     <div>
       <HeaderSummary></HeaderSummary>
+      <ProjectComponent></ProjectComponent>
       <AddProjectButtonComponent></AddProjectButtonComponent>
     </div>
   );
