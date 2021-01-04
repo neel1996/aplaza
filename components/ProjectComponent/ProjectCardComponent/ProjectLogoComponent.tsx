270643cf54a166aca97100d621d3860495ba5f91
Neel <neelusenthil1996@gmail.com>
2021-01-04 21:16:22 +0530

    add conditional logic to handle null props

--- components/ProjectComponent/ProjectCardComponent/ProjectLogoComponent.tsx
+++ components/ProjectComponent/ProjectCardComponent/ProjectLogoComponent.tsx
@@ -5,7 +5,7 @@ export default function ProjectLogoComponent(props: {
 }) {
   return (
     <div className="px-10 py-20 mx-auto project-initial xl:w-1/5 lg:w-1/5 w-1/4 hidden xl:block lg:block rounded-l-lg text-5xl text-center text-white">
-      {props.projectInitial.toUpperCase()}
+      {props.projectInitial ? props.projectInitial.toUpperCase() : ""}
     </div>
   );
 }
