451be15aa887031601a4aa2466ba9fc25c5da191
Neel <neelusenthil1996@gmail.com>
2021-01-04 23:08:51 +0530

    add new data file for populating content

--- components/ProjectInfoComponent/ProjectInfoComponent.tsx
+++ components/ProjectInfoComponent/ProjectInfoComponent.tsx
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import { projectDataType } from "./ProjectDataTypeDefenition";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCalendar, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import ProjectStatusComponent from "../ProjectComponent/ProjectCardComponent/ProjectDueDateComponent/ProjectStatusComponent";
+import DeleteProjectComponent from "./DeleteProjectComponent";
+import { projectDataType } from "./ProjectDataTypeDefenition";
+import { ProjectInfoData } from "./projectInfoData";
 
 export default function ProjectInfoComponent(props: { projectId: string }) {
   const [projectData, setProjectData] = useState<projectDataType>({
@@ -14,6 +15,8 @@ export default function ProjectInfoComponent(props: { projectId: string }) {
     projectDueDate: "",
     projectRepoURL: "",
   });
+  const [initiateDelete, setInitiateDelete] = useState(false);
+  const [fetchError, setFetchError] = useState(false);
 
   useEffect(() => {
     if (props.projectId) {
@@ -31,55 +34,79 @@ export default function ProjectInfoComponent(props: { projectId: string }) {
         })
         .catch((err) => {
           console.log(err);
+          setFetchError(true);
         });
     }
   }, [props.projectId]);
+
+  function setDeletePopUpState(status: false) {
+    setInitiateDelete(status);
+  }
+
   return (
-    <div className="my-10 mx-auto w-3/4 h-full pt-10 rounded-lg bg-white shadow-lg border border-blue-100">
-      <div className="mx-10 project-title flex w-1/3">
-        <div className="text-4xl font-semibold font-sans text-gray-700">
-          {projectData.projectName}
+    <div className="my-10 mx-auto w-11/12 xl:w-3/4 lg:w-3/4 h-full rounded-lg bg-white shadow-lg border border-blue-100">
+      {fetchError ? (
+        <div className="text-center p-10 mx-auto font-semibold font-sans text-pink-300 text-2xl border-2 border-dashed border-pink-500 rounded-lg">
+          {props.projectId} could not be found in the data store!
         </div>
-        <ProjectStatusComponent
-          projectDueDate={projectData.projectDueDate}
-        ></ProjectStatusComponent>
-      </div>
-      <div className="mx-14 my-10">
-        <div className="flex items-center justify-start w-full my-6">
-          <div className="flex items-center w-1/3 text-blue-300 text-3xl">
-            <div>
-              <FontAwesomeIcon icon={faInfoCircle}></FontAwesomeIcon>
+      ) : (
+        <>
+          <div className="mx-4 my-10 xl:mx-10 lg:mx-10 project-title flex w-full xl:w-1/3 lg:w-1/3">
+            <div className="text-4xl font-semibold font-sans text-gray-700">
+              {projectData.projectName}
             </div>
-            <div className="mx-4 font-semibold font-sans border-b text-xl">
-              DESCRIPTION
+            <ProjectStatusComponent
+              projectDueDate={projectData.projectDueDate}
+            ></ProjectStatusComponent>
+          </div>
+          <div className="mx-5 xl:mx-14 lg:mx-14 md:mx-10 my-10">
+            {projectData &&
+              ProjectInfoData &&
+              ProjectInfoData.map((infoData) => {
+                const { icon, label } = infoData;
+
+                return (
+                  <div
+                    className="block xl:flex lg:flex items-center justify-start w-full my-6"
+                    key={label}
+                  >
+                    <div className="flex items-center w-full xl:w-1/3 lg:w-1/3 text-blue-300 text-3xl">
+                      <div>
+                        <FontAwesomeIcon icon={icon}></FontAwesomeIcon>
+                      </div>
+                      <div className="mx-4 font-semibold font-sans border-b text-xl">
+                        {label}
+                      </div>
+                    </div>
+                    <div className="w-full xl:w-4/5 lg:w-4/5 my-3 xl:my-0 lg:my-0 text-xl xl:text-2xl font-light font-sans text-gray-800">
+                      {projectData.projectDescription}
+                    </div>
+                  </div>
+                );
+              })}
+          </div>
+          <div className="mt-10 flex w-full mx-auto justify-between items-center">
+            <div
+              className="w-1/2 cursor-pointer rounded-bl-lg text-base xl:text-xl lg:text-xl text-white p-4 bg-gradient-to-t from-pink-400 to-pink-500 shadow-inner hover:to-pink-400 font-sans font-semibold text-center"
+              onClick={(e) => {
+                setInitiateDelete(true);
+              }}
+            >
+              DELETE
+            </div>
+            <div className="w-1/2 cursor-pointer rounded-br-lg text-base xl:text-xl lg:text-xl text-white p-4 bg-gradient-to-t from-green-400 to-green-500 shadow-inner hover:to-green-400 font-sans font-semibold text-center">
+              MARK AS COMPLETE
             </div>
           </div>
-          <div className="w-4/5 text-2xl font-light font-sans text-gray-800">
-            {projectData.projectDescription}
-          </div>
-        </div>
-        <div className="flex items-center justify-start w-full my-6">
-          <div className="flex items-center w-1/3 text-blue-300 text-3xl">
-            <div>
-              <FontAwesomeIcon icon={faCalendar}></FontAwesomeIcon>
-            </div>
-            <div className="mx-4 font-semibold font-sans border-b text-xl">
-              DUE DATE
-            </div>
-          </div>
-          <div className="w-4/5 text-2xl font-light font-sans text-gray-800">
-            {projectData.projectDueDate}
-          </div>
-        </div>
-      </div>
-      <div className="mt-10 flex w-full mx-auto justify-between">
-        <div className="w-1/2 cursor-pointer rounded-bl-lg text-xl text-white p-4 bg-gradient-to-t from-pink-400 to-pink-500 shadow-inner hover:to-pink-400 font-sans font-semibold text-center">
-          DELETE
-        </div>
-        <div className="w-1/2 cursor-pointer rounded-br-lg text-xl text-white p-4 bg-gradient-to-t from-green-400 to-green-500 shadow-inner hover:to-green-400 font-sans font-semibold text-center">
-          MARK AS COMPLETED
-        </div>
-      </div>
+        </>
+      )}
+
+      {initiateDelete && projectData.projectId ? (
+        <DeleteProjectComponent
+          projectId={projectData.projectId}
+          setDeleteStatus={setDeletePopUpState}
+        ></DeleteProjectComponent>
+      ) : null}
     </div>
   );
 }
---
+++ components/ProjectInfoComponent/projectInfoData.ts
@@ -0,0 +1,12 @@
+import { faInfoCircle, faCalendar } from "@fortawesome/free-solid-svg-icons";
+
+export const ProjectInfoData = [
+  {
+    icon: faInfoCircle,
+    label: "DESCRIPTION",
+  },
+  {
+    icon: faCalendar,
+    label: "DUE DATE",
+  },
+];
