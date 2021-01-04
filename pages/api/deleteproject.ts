538d367bef6c1cdd54a646728f2a5d8d05b1905a
Neel <neelusenthil1996@gmail.com>
2021-01-04 23:09:42 +0530

    add component and logic to delete a project

---
+++ pages/api/deleteproject.ts
@@ -0,0 +1,24 @@
+import { NextApiResponse } from "next";
+import { NextApiRequest } from "next";
+import { StorageConnectionFactory } from "./data/storageConnectorFactory/StorageConnectorFactory";
+export default async function deleteProject(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const projectId: string = "req.body.projectId";
+  const storageConnector = new StorageConnectionFactory().getStorageConnector();
+  const deletedProject = await storageConnector.deleteProject(projectId);
+
+  if (deletedProject && deletedProject.projectId) {
+    res.statusCode = 204;
+    res.json({
+      projectId: projectId,
+      message: "Project deleted from the data store",
+    });
+  } else {
+    res.statusCode = 404;
+    res.json({
+      message: "Project could not be located in the datastore",
+    });
+  }
+}
